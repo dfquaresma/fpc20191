5622ccda69176d355706491506d3c2f0813ba7a8
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-05-24 14:51:38 -0300

    Fix function print

--- q4/q4a-impl.java
+++ q4/q4a-impl.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class Main {
 
     public static void main(String args[]) {
-        System.out.println(gateway(5));
+        System.out.println("gateway=" + gateway(5));
     }
 
     public static int gateway(int numReplicas) {
