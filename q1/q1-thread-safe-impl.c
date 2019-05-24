ea73030dcbc6e3701b5d5135e8c3757da6903ea8
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-05-24 14:52:40 -0300

    Retype q1 from cpp to c

--- q1/q1-thread-safe-impl.c
+++ q1/q1-thread-safe-impl.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <pthread.h>
 
+pthread_mutex_t mutex;
 long int counter = 0;
 
 void* run (void* args) {
@@ -9,10 +10,11 @@ void* run (void* args) {
     long int j;
 
     my_id = (int) args;
-
+    pthread_mutex_lock(&mutex); 
     for (j = 0; j < 1e7; j++) {
         counter = counter + 1;
     }
+    pthread_mutex_unlock(&mutex); 
 
     printf("my_id=%d j=%ld counter=%ld\n", my_id, j, counter);
     pthread_exit(my_id);
@@ -21,13 +23,14 @@ void* run (void* args) {
 int main (int argc, char *argv[]) {
     int i;
     pthread_t pthreads[3];
-    
+
+    pthread_mutex_init(&mutex, NULL);
     for (i = 0; i < 3; i++) {
-        pthread_create (&pthreads[i], NULL, &run, (void*) i);
+        pthread_create(&pthreads[i], NULL, &run, (void*) i);
     }
 
     for (i = 0; i < 3; i++) {
-        pthread_create (&pthreads[i], NULL);
+        pthread_join(&pthreads[i], NULL);
     }
 
     printf("counter=%ld\n", counter);
