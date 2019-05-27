fb48da112adf166fc4bf48225a6c0cb339a508c7
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-05-27 19:34:36 -0300

    Update q4a's semaphore version impl

--- q4/q4aSemaphoreImpl.java
+++ q4/q4aSemaphoreImpl.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.concurrent.Semaphore;
 
 public class Main {
 
@@ -8,45 +9,52 @@ public class Main {
 
     public static int gateway(int numReplicas) {
         First first = new First();
+        Semaphore semaphore = new Semaphore(0);
         for (int i = 0; i < numReplicas; i++) {
-            Request request = new Request(first);
+            Request request = new Request(semaphore, first);
             Thread thread = new Thread(request, "request" + i);
             thread.start();
         }
 
-        Timer timer = new Timer(first);
+        Timer timer = new Timer(semaphore, first);
         Thread timerThread = new Thread(timer, "timer");
         timerThread.start();
     
-        synchronized (first) {
-            try {
-                first.wait();
-            } catch (InterruptedException e) {}
-            return first.get();
-        }
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {}
+        return first.get();
     }
 
+
     public static class Timer implements Runnable {
+        private Semaphore semaphore;
         private First first;
 
-        public Timer(First first) {
+        public Timer(Semaphore semaphore, First first) {
+            this.semaphore = semaphore;
             this.first = first;
         }
-        
+
         @Override
         public void run() {
             try {
                 Thread.sleep(8 * 1000); // Thread.sleep sleeps milliseconds
             } catch (InterruptedException e) {}
-            this.first.set(-1);
+            synchronized(this.first) {
+                this.first.set(-1);
+                this.semaphore.release();
+            }
         }
     }
 
     public static class Request implements Runnable {
+        private Semaphore semaphore;
         private First first;
         private int randomNumber;
 
-        public Request (First first) {
+        public Request (Semaphore semaphore, First first) {
+            this.semaphore = semaphore;
             this.first = first;
             this.randomNumber = (new Random()).nextInt(30) + 1; // Obtain a number between [1 - 30]. 
         }
@@ -56,6 +64,7 @@ public class Main {
                 Thread.sleep(this.randomNumber * 1000); // Thread.sleep sleeps milliseconds
             } catch (InterruptedException e) {}
             this.first.set(this.randomNumber);
+            this.semaphore.release();
         }
     }
 
@@ -67,7 +76,6 @@ public class Main {
             if (!this.set) {
                 this.set = true;
                 this.value = value;
-                this.notifyAll();
             }
         }
 
