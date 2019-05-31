e5fdb3786233b6bdbc58cb961b6fd61a10c66527
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-05-30 21:28:36 -0300

    Update q3a-cond-impl.c

--- q3/q3a-cond-impl.c
+++ q3/q3a-cond-impl.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <assert.h>
 #include <pthread.h>
+#include <stdbool.h>
 
 pthread_mutex_t mutex;
-pthread_cond_t first;
-int firstToWake = 0;
+pthread_cond_t cond;
+int first_to_wake_value = 0;
 
 void *request (void *args) {
     int random_number = (rand() % 30) + 1; // // Obtain a number between [1 - 30].
     sleep(random_number); // Sleeps seconds
 
     pthread_mutex_lock(&mutex);
-    if (firstToWake == 0) {
-        firstToWake = random_number;
+    if (first_to_wake_value == 0) {
+        first_to_wake_value = random_number;
     }
-    pthread_cond_signal(&first);
+    pthread_cond_signal(&cond);
     pthread_mutex_unlock(&mutex);
     
     pthread_exit(random_number);
@@ -23,18 +24,20 @@ void *request (void *args) {
 int gateway (int num_replicas) {
     pthread_t pthreads[num_replicas];
     for (int i = 0; i < num_replicas; i++) {
-        pthread_create (&pthreads[i], NULL, &request, (void*) i);
+        pthread_create(&pthreads[i], NULL, &request, (void*) i);
     }
     int valueToReturn;
     pthread_mutex_lock(&mutex);
-    pthread_cond_wait(&first, &mutex);
-    valueToReturn = firstToWake;
+    while (first_to_wake_value == 0) {
+        pthread_cond_wait(&cond, &mutex);
+    }
+    valueToReturn = first_to_wake_value;
     pthread_mutex_unlock(&mutex);
     return valueToReturn;
 }
 
 int main (int argc, char *argv[]) {
     pthread_mutex_init(&mutex, NULL);
-    pthread_cond_init(&first, NULL);
+    pthread_cond_init(&cond, NULL);
     printf("gateway=%d\n", gateway(5));
 }
