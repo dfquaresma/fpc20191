017dde04fe8268c34b14dfdc056ddc5b3cc6870a
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-05-30 22:10:04 -0300

    Finish q4a c impl

--- q4/q4a-semaphore-impl.c
+++ q4/q4a-semaphore-impl.c
@@ -2,34 +2,39 @@
 #include <assert.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <time.h>
 
 sem_t semaphore;
 pthread_mutex_t mutex;
 int numberOfThreads = 5;
-int firstToWake = 0;
+int first_to_wake_value = 0;
 
 void *timer (void *args) {
+    printf("Timer will sleep 8 seconds.\n");
     sleep(8); // Sleeps seconds
+
     pthread_mutex_lock(&mutex);
-    if (firstToWake == 0) {
-        firstToWake = -1;
+    if (first_to_wake_value == 0) {
+        first_to_wake_value = -1;
     }
     sem_post(&semaphore);
     pthread_mutex_unlock(&mutex);
+    
     pthread_exit(NULL);
 }
 
 void *request (void *args) {
     int random_number = (rand() % 30) + 1; // // Obtain a number between [1 - 30].
+    printf("Request will sleep %d seconds\n", random_number);
     sleep(random_number); // Sleeps seconds
 
     pthread_mutex_lock(&mutex);
-    if (firstToWake == 0) {
-        firstToWake = random_number;
+    if (first_to_wake_value == 0) {
+        first_to_wake_value = random_number;
     }
     sem_post(&semaphore);
-    printf("REQ: %d", random_number);
     pthread_mutex_unlock(&mutex);
+
     pthread_exit(random_number);
 }
 
@@ -38,18 +43,18 @@ int gateway (int num_replicas) {
     for (int i = 0; i < num_replicas; i++) {
         pthread_create(&pthreads[i], NULL, &request, (void*) i);
     }
-    
     pthread_t timerThread;
     pthread_create(&timerThread, NULL, &timer, NULL);
 
     sem_wait(&semaphore);
     pthread_mutex_lock(&mutex);
-    int firstValue = firstToWake;
+    int valueToReturn = first_to_wake_value;
     pthread_mutex_unlock(&mutex);
-    return firstValue;
+    return valueToReturn;
 }
 
 int main (int argc, char *argv[]) {
+    srand ( time(NULL) );
     sem_init(&semaphore, 0, 0);
     pthread_mutex_init(&mutex, NULL);
     printf("gateway=%d\n", gateway(numberOfThreads));
