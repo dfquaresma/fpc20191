b979f84a471713137fd78a4d96083ff24d23bac8
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-05-30 22:27:46 -0300

    Update q4b-semaphore-impl.c

--- q4/q4b-semaphore-impl.c
+++ q4/q4b-semaphore-impl.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <time.h>
 
 sem_t semaphore;
 pthread_mutex_t mutex;
@@ -11,33 +12,47 @@ int threadsFinished = 0;
 int sum = 0;
 
 void *joiner (void *args) {
+    int joiner_sum = 0;
+    printf("Joiner waiting...\n");
     for (int i = 0; i < numberOfThreads; i++) {
-        pthread_join(pthreads[i], NULL);
+        int aux = 0;
+        pthread_join(pthreads[i], &aux);    
+        printf("Thread %d finished.\n", i);
+        joiner_sum += aux;        
+    }
+
+    pthread_mutex_lock(&mutex);
+    if (sum != -1) {
+        sum = joiner_sum;
     }
     sem_post(&semaphore);
+    pthread_mutex_unlock(&mutex);
+    
     pthread_exit(NULL);
 }
 
 void *timer (void *args) {
+    printf("Timer will sleep 16 seconds.\n");
     sleep(16); // Sleeps seconds
+
     pthread_mutex_lock(&mutex);
     if (threadsFinished < numberOfThreads) {
         sum = -1;
     }
     sem_post(&semaphore);
     pthread_mutex_unlock(&mutex);
+
     pthread_exit(NULL);
 }
 
 void *request (void *args) {
     int random_number = (rand() % 30) + 1; // // Obtain a number between [1 - 30].
-    sleep(random_number); // Sleeps seconds
-
+    printf("Request will sleep %d seconds\n", random_number);
+    sleep(random_number); // Sleeps seconds  
+    
     pthread_mutex_lock(&mutex);
-    if (sum != -1) {
-        sum += random_number;
-    }
-    pthread_mutex_unlock(&mutex);
+    threadsFinished++; 
+    pthread_mutex_unlock(&mutex);  
     
     pthread_exit(random_number);
 }
@@ -60,6 +75,7 @@ int gateway (int num_replicas) {
 }
 
 int main (int argc, char *argv[]) {
+    srand ( time(NULL) );
     sem_init(&semaphore, 0, 0);
     pthread_mutex_init(&mutex, NULL);
     printf("gateway=%ld\n", gateway(numberOfThreads));
