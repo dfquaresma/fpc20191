89d92d89f5773d6ebb10f7add22a69a2cf671128
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-06-02 13:28:50 -0300

    Update run.sh

--- q5/run.sh
+++ q5/run.sh
@@ -16,19 +16,17 @@ echo "READ_RATE: ${READ_RATE:= 0 0.25 0.5 0.75 1}"
 # Compiling the stuff
 javac Main.java
 
+mkdir -p results/
 for expid in `seq ${EXPID_START} ${EXPID_END}`;
 do
-    echo ${expid}
     for structure_option in ${STRUCTURE_OPTION};
     do
-        echo ${structure_option}
         for number_of_threads in ${NUMBER_OF_THREADS};
         do
-            echo ${number_of_threads}
             for read_rate in ${READ_RATE};
             do
-                echo ${read_rate}
-                java Main ${structure_option} ${number_of_threads} ${read_rate}
+                echo -e "${GREEN}EXPERIMENT RUNNING: ${structure_option}-t${number_of_threads}-r${read_rate}-e${expid}${NC}"
+                java Main ${structure_option} ${number_of_threads} ${read_rate} > results/${structure_option}-t${number_of_threads}-r${read_rate}-e${expid}.csv
             done;
         done;
     done;
