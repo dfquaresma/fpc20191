a80e80d833eacfa8dd87e43ae57f11995e068861
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-06-02 12:23:17 -0300

    Fix scritpExe

--- q5/scriptExe.sh
+++ q5/scriptExe.sh
@@ -2,10 +2,15 @@
 date
 set -x
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
 echo "EXPID_START: ${EXPID_START:=0}"
 echo "EXPID_END: ${EXPID_END:=4}"
 echo "STRUCTURE_OPTION: ${STRUCTURE_OPTION:= concurrentHash synchronizedMap copyOnWriteList synchronizedList}"
-echo "NUMBER_OF_THREADS: ${READ_RATE:= 1 128 512}"
+echo "NUMBER_OF_THREADS: ${NUMBER_OF_THREADS:= 1 128 512}"
 echo "READ_RATE: ${READ_RATE:= 0 0.25 0.5 0.75 1}"
 
 for expid in `seq ${EXPID_START} ${EXPID_END}`;
@@ -20,7 +25,7 @@ do
             for read_rate in ${READ_RATE};
             do
                 echo ${read_rate}
-                java Main ${structure_option} ${number_of_threads} ${read_rate} > exp${expid}-${structure_option}-threads${number_of_threads}-rate${read_rate}.log
+                java Main ${structure_option} ${number_of_threads} ${read_rate}
             done;
         done;
     done;
