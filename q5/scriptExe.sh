6a1fcac6ebbb732c1056cb814d3b85cea8a33818
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-06-02 12:14:36 -0300

    Update shell

--- q5/scriptExe.sh
+++ q5/scriptExe.sh
@@ -8,20 +8,20 @@ echo "STRUCTURE_OPTION: ${STRUCTURE_OPTION:= concurrentHash synchronizedMap copy
 echo "NUMBER_OF_THREADS: ${READ_RATE:= 1 128 512}"
 echo "READ_RATE: ${READ_RATE:= 0 0.25 0.5 0.75 1}"
 
-for expid in `seq ${EXPID_START} ${EXPID_END}`
+for expid in `seq ${EXPID_START} ${EXPID_END}`;
 do
     echo ${expid}
-    for structure_option in ${STRUCTURE_OPTION}
+    for structure_option in ${STRUCTURE_OPTION};
     do
         echo ${structure_option}
-        for number_of_threads in ${NUMBER_OF_THREADS}
+        for number_of_threads in ${NUMBER_OF_THREADS};
         do
-            echo ${number_of_threads}   
-            for read_rate in ${READ_RATE}
+            echo ${number_of_threads}
+            for read_rate in ${READ_RATE};
             do
                 echo ${read_rate}
                 java Main ${structure_option} ${number_of_threads} ${read_rate} > exp${expid}-${structure_option}-threads${number_of_threads}-rate${read_rate}.log
-            done
-        done
-    done
+            done;
+        done;
+    done;
 done
