2b6b88ce8b4368c7fd14ed814f4652c11be87c7e
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-06-02 12:05:23 -0300

    Update scriptExe.sh

--- q5/scriptExe.sh
+++ q5/scriptExe.sh
@@ -1,51 +1,23 @@
-for i in 1 2 3 4 5
+#!/bin/bash
+date
+set -x
+
+echo "EXPID_START: ${EXPID_START:=0}"
+echo "EXPID_END: ${EXPID_END:=4}"
+echo "READ_RATE: ${READ_RATE:= 0 0.25 0.5 0.75 1}"
+echo "STRUCTURE_OPTION: ${STRUCTURE_OPTION:= concurrentHash synchronizedMap copyOnWriteList synchronizedList}"
+echo "NUMBER_OF_THREADS: ${READ_RATE:= 1 128 512}"
+
+for expid in `seq ${EXPID_START} ${EXPID_END}`
 do
-java Main concurrentHash 1 0.25 >> dadosDasEstruturas.txt
-java Main concurrentHash 1 0.5 >> dadosDasEstruturas.txt
-java Main concurrentHash 1 0.75 >> dadosDasEstruturas.txt
-java Main concurrentHash 1 1 >> dadosDasEstruturas.txt
-java Main concurrentHash 128 0.25 >> dadosDasEstruturas.txt
-java Main concurrentHash 128 0.5 >> dadosDasEstruturas.txt
-java Main concurrentHash 128 0.75 >> dadosDasEstruturas.txt
-java Main concurrentHash 128 1 >> dadosDasEstruturas.txt
-java Main concurrentHash 512 0.25 >> dadosDasEstruturas.txt
-java Main concurrentHash 512 0.5 >> dadosDasEstruturas.txt
-java Main concurrentHash 512 0.75 >> dadosDasEstruturas.txt
-java Main concurrentHash 512 1 >> dadosDasEstruturas.txt
-java Main synchronizedMap 1 0.25 >> dadosDasEstruturas.txt
-java Main synchronizedMap 1 0.5 >> dadosDasEstruturas.txt
-java Main synchronizedMap 1 0.75 >> dadosDasEstruturas.txt
-java Main synchronizedMap 1 1 >> dadosDasEstruturas.txt
-java Main synchronizedMap 128 0.25 >> dadosDasEstruturas.txt
-java Main synchronizedMap 128 0.5 >> dadosDasEstruturas.txt
-java Main synchronizedMap 128 0.75 >> dadosDasEstruturas.txt
-java Main synchronizedMap 128 1 >> dadosDasEstruturas.txt
-java Main synchronizedMap 512 0.25 >> dadosDasEstruturas.txt
-java Main synchronizedMap 512 0.5 >> dadosDasEstruturas.txt
-java Main synchronizedMap 512 0.75 >> dadosDasEstruturas.txt
-java Main synchronizedMap 512 1 >> dadosDasEstruturas.txt
-java Main copyOnWriteList 1 0.25 >> dadosDasEstruturas.txt
-java Main copyOnWriteList 1 0.5 >> dadosDasEstruturas.txt
-java Main copyOnWriteList 1 0.75 >> dadosDasEstruturas.txt
-java Main copyOnWriteList 1 1 >> dadosDasEstruturas.txt
-java Main copyOnWriteList 128 0.25 >> dadosDasEstruturas.txt
-java Main copyOnWriteList 128 0.5 >> dadosDasEstruturas.txt
-java Main copyOnWriteList 128 0.75 >> dadosDasEstruturas.txt
-java Main copyOnWriteList 128 1 >> dadosDasEstruturas.txt
-java Main copyOnWriteList 512 0.25 >> dadosDasEstruturas.txt
-java Main copyOnWriteList 512 0.5 >> dadosDasEstruturas.txt
-java Main copyOnWriteList 512 0.75 >> dadosDasEstruturas.txt
-java Main copyOnWriteList 512 1 >> dadosDasEstruturas.txt
-java Main synchronizedList 1 0.25 >> dadosDasEstruturas.txt
-java Main synchronizedList 1 0.5 >> dadosDasEstruturas.txt
-java Main synchronizedList 1 0.75 >> dadosDasEstruturas.txt
-java Main synchronizedList 1 1 >> dadosDasEstruturas.txt
-java Main synchronizedList 128 0.25 >> dadosDasEstruturas.txt
-java Main synchronizedList 128 0.5 >> dadosDasEstruturas.txt
-java Main synchronizedList 128 0.75 >> dadosDasEstruturas.txt
-java Main synchronizedList 128 1 >> dadosDasEstruturas.txt
-java Main synchronizedList 512 0.25 >> dadosDasEstruturas.txt
-java Main synchronizedList 512 0.5 >> dadosDasEstruturas.txt
-java Main synchronizedList 512 0.75 >> dadosDasEstruturas.txt
-java Main synchronizedList 512 1 >> dadosDasEstruturas.txt
+    for structure_option in ${STRUCTURE_OPTION}
+    do
+        for number_of_threads in ${NUMBER_OF_THREADS}
+        do
+            for read_rate in ${READ_RATE}
+            do
+                java Main ${structure_option} ${number_of_threads} ${read_rate} > exp${expid}-${structure_option}-threads${number_of_threads}-rate${read_rate}.log
+            done
+        done
+    done
 done
