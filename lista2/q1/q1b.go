0f438d72eabc1968c1c82b7d9e8d29b249fde90d
renatodh <renato.henriques@ccc.ufcg.edu.br>
2019-06-26 20:04:46 -0300

    Add folder for q1.

---
+++ lista2/q1/q1b.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main(){
+	fmt.Println(gateway(10))
+}
+
+func request(c chan int) int{
+	rand.Seed(time.Now().UnixNano())
+	randNumber := rand.Intn(30)	+ 1
+	//fmt.Println("A função irá dormir por", randNumber, "segundos.")
+	time.Sleep(time.Duration(randNumber) * time.Second)
+	//fmt.Println(randNumber, "terminou.")
+	c <- randNumber
+	
+	return randNumber
+}
+
+func gateway(num_replicas int) int{
+	ch0 := make(chan int)
+	soma := 0
+	
+	for i := 0; i < num_replicas; i++ {
+		//time.Sleep(1000) //para que o seed mude
+        go request(ch0)
+    }
+
+    for i := 0; i < num_replicas; i++ {
+        soma += <- ch0
+    }
+
+    return soma
+}
