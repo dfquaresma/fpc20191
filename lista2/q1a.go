3aba6e0061a54209295c5b0c05c7d5a0b53a964c
renatodh <renato.henriques@ccc.ufcg.edu.br>
2019-06-26 20:02:22 -0300

    Correcting q1.

--- lista2/q1a.go
+++ lista2/q1a.go
@@ -7,9 +7,7 @@ import (
 )
 
 func main(){
-	ch0 := make(chan int)
-	gateway(50, ch0)
-	fmt.Println(<-ch0)
+	fmt.Println(gateway(10))	
 }
 
 func request(c chan int) int{
@@ -19,12 +17,17 @@ func request(c chan int) int{
 	time.Sleep(time.Duration(randNumber) * time.Second)
 	//fmt.Println(randNumber, "terminou.")
 	c <- randNumber
+
 	return randNumber
 }
 
-func gateway(num_replicas int, c chan int){
+func gateway(num_replicas int) int{
+	ch0 := make(chan int)
+	
 	for i := 0; i < num_replicas; i++ {
 		//time.Sleep(1000) //para que o seed mude 
-        go request(c)
+        go request(ch0)
     }
+
+    return <-ch0
 }
--- lista2/q1b.go
+++ lista2/q1b.go
@@ -7,14 +7,7 @@ import (
 )
 
 func main(){
-	num_replicas := 10
-	c := make(chan int)
-	gateway(num_replicas, c)
-	soma := 0
-	for i := 0; i < num_replicas; i++ {
-        soma += <- c
-    }
-    fmt.Println(soma)
+	fmt.Println(gateway(10))
 }
 
 func request(c chan int) int{
@@ -24,12 +17,22 @@ func request(c chan int) int{
 	time.Sleep(time.Duration(randNumber) * time.Second)
 	//fmt.Println(randNumber, "terminou.")
 	c <- randNumber
+	
 	return randNumber
 }
 
-func gateway(num_replicas int, c chan int){
+func gateway(num_replicas int) int{
+	ch0 := make(chan int)
+	soma := 0
+	
 	for i := 0; i < num_replicas; i++ {
 		//time.Sleep(1000) //para que o seed mude
-        go request(c)
+        go request(ch0)
     }
+
+    for i := 0; i < num_replicas; i++ {
+        soma += <- ch0
+    }
+
+    return soma
 }
