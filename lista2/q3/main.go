62c5b88969975f8aedbe633ecf389e02c9f2c329
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-06-28 15:10:25 -0300

    Add main impl for go and java

---
+++ lista2/q3/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main(){
+	fmt.Println(gateway(10))	
+}
+
+func request(c chan int) int{
+	rand.Seed(time.Now().UnixNano())
+	randNumber := rand.Intn(30)	+ 1
+	fmt.Println("A função irá dormir por ", randNumber, " segundos.")
+	time.Sleep(time.Duration(randNumber) * time.Second)
+	c <- randNumber
+
+	return randNumber
+}
+
+func gateway(num_replicas int) int{
+	ch0 := make(chan int)
+	
+	for i := 0; i < num_replicas; i++ {
+        go request(ch0)
+    }
+
+    return <-ch0
+}
