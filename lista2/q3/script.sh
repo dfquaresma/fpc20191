7f6ce1b7dfafd86d5ac17572c58fd2c6fe9bb3e9
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-07-02 23:11:37 -0300

    Update script.sh

--- lista2/q3/script.sh
+++ lista2/q3/script.sh
@@ -5,8 +5,8 @@ set -x
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-echo "NUMBER_OF_THREADS: ${NUMBER_OF_THREADS:=0 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768}"
-echo "NUMBER_OF_EXECUTION: ${NUMBER_OF_EXECUTION:=5}"
+echo "NUMBER_OF_THREADS: ${NUMBER_OF_THREADS:=0 1 2 4 8 16 32 64 128 256 512 1024 2048}" # 4096 8192 16384 32768}, after 4096, java throw java.lang.OutOfMemoryError.
+echo "NUMBER_OF_EXECUTION: ${NUMBER_OF_EXECUTION:=1}"
 
 # Compiling the stuff
 javac Main.java
@@ -15,26 +15,30 @@ mkdir -p results/
 # Running for Java
 for number_of_threads in ${NUMBER_OF_THREADS};
 do
-    echo "memory_size_in_K" >> results/java-${number_of_threads}.csv
+    FILE_PATH = "results/java-${number_of_threads}.csv"
+    echo "memory_size_in_K" >> ${FILE_PATH}
     for expid in `seq 1 ${NUMBER_OF_EXECUTION}`;
     do
         echo -e "${GREEN}EXPERIMENT RUNNING: Java ${number_of_threads} threads${NC}"
         java Main ${number_of_threads} &
-	sleep 1
-        ps v --pid=$! | tail -n 1 | awk -F " " '{ print $8 }' >> results/java-${number_of_threads}.csv
+	    sleep 1
+        echo -n "${number_of_threads}," ${FILE_PATH}
+        ps v --pid=$! | tail -n 1 | awk -F " " '{ print $8 }' >> ${FILE_PATH}
     done;
 done;
 
 # Running for Go
 for number_of_goroutines in ${NUMBER_OF_THREADS};
 do
-    echo "memory_size_in_K" >> results/go-${number_of_goroutines}.csv
+    FILE_PATH = "results/go-${number_of_threads}.csv"
+    echo "memory_size_in_K" >> ${FILE_PATH}
     for expid in `seq 1 ${NUMBER_OF_EXECUTION}`;
     do
         echo -e "${GREEN}EXPERIMENT RUNNING: Go ${number_of_goroutines} goroutines${NC}"
         go run main.go ${number_of_goroutines} &
-	sleep 1
-        ps v --pid=$! | tail -n 1 | awk -F " " '{ print $8 }' >> results/go-${number_of_goroutines}.csv
+	    sleep 1
+        echo -n "${number_of_goroutines}," ${FILE_PATH}
+        ps v --pid=$! | tail -n 1 | awk -F " " '{ print $8 }' >> ${FILE_PATH}
     done;
 done;
 
